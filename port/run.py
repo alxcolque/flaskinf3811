7869eaef6afbecd250f5e96a83458aa9a6ce8dd4
unknown <a.colq.c@hotmail.com>
2021-03-17 19:04:35 -0400

    Ruta con parametros

--- port/run.py
+++ port/run.py
@@ -3,5 +3,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return ("Hola mundo!")
+@app.route("/saludo")
+def saludo():
+    return ("Hola !")
+
+@app.route("/usuario/<string:usuario>")
+def usuario():
+    return "Hola: "+usuario
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
